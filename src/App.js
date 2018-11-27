67eb7b769af4694748e1f1c3f9cecb6f9bd81226
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-26 23:51:01 -1000

    Updated App.js to include a constructor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+
+//Import JSX component files
+
+
+//Setup for redux
+// import { connect } from 'react-redux';
+// import { getAllTasks } from './actions/actions.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    //State is an object, React handles state to do updates
+    this.state = {
+
+    }
+  }
+
+  //Lifecycle Methods
+  componentDidMount() {
+
+  }
+
+  //App Component - functions
+  //Never mutate 'state' directly, use 'this.setState' (use ES6 to bind)
+
+
+
+
+
+  //App Component - render html elements
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            WORKING??!?!!?
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Trekmate</h1>
         </header>
       </div>
     );
   }
 }
 
+//Export App component so other components can use it
 export default App;
