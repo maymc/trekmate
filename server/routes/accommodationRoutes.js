c3ae981336da6b34c4d9d2da7aa7ecab2e99bc7e
Abby Zhang <abbyzhang@Abbys-MBP.home>
2018-11-28 22:02:02 -1000

    📦  NEW: accommodation add is working

--- server/routes/accommodationRoutes.js
+++ server/routes/accommodationRoutes.js
@@ -34,7 +34,36 @@ accommodationRouter.get('/:id', (req, res) => {
 })
 
 //post new Accommodation into 'Accommodation' table
+accommodationRouter.post('/add', (req, res) => {
+    const newAccommodation = {
+        lodging_name: req.body.lodging_name,
+        address: req.body.address,
+        check_in_date: req.body.check_in_date,
+        check_out_date: req.body.check_out_date,
+        price: req.body.price,
+        is_paid: req.body.is_paid,
+        reservation_code: req.body.reservation_code,
+        notes: req.body.notes,
+        user_id: req.body.user_id,
+        trip_id: req.body.trip_id
+    }
 
+    // console.log("\nNew User check:", newAccommodation);
+
+    Accommodation
+        .forge(newAccommodation)
+        .save()
+        .then(() => {
+            return Accommodation.fetchAll()
+        })
+        .then(Accommodation => {
+            res.json(Accommodation.serialize());
+        })
+        .catch(err => {
+            console.log("\nPOST - adding new user error", err);
+            res.json("error");
+        })
+});
 
 
 module.exports = accommodationRouter
