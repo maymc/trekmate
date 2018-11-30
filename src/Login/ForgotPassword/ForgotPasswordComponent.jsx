d1f9cd01b8da7a86d328b6e2c4224c2701218eb1
May Chen <maychen@Mays-MacBook-Pro.local>
2018-11-29 21:33:50 -1000

    Updated forgotPasswordComponent helper functions

--- src/Login/ForgotPassword/ForgotPasswordComponent.jsx
+++ src/Login/ForgotPassword/ForgotPasswordComponent.jsx
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
+// import axios from 'axios';
+// import { connect } from 'react-redux';
 
 class ForgotPassword extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      firstName: null,
-      lastName: null,
-      email: null,
-      password: null
+      email: null
     }
   }
 
   //Lifecycle Methods
   componentDidMount() { }
 
+  //Helper Functions
   handleChange = (e) => {
     e.preventDefault();
+
     const { name, value } = e.target;
     this.setState({
       [name]: value
     })
-    console.log("Forgot password - handleChange this.state:", this.state);
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("User forgot password submitted!", this.state);
+    console.log("Email sent to user for forgotten password", this.state);
   }
 
   render() {
@@ -45,6 +43,5 @@ class ForgotPassword extends Component {
     )
   }
 }
-
 
 export default ForgotPassword;
