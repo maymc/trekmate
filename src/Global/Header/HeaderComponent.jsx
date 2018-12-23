713ff9507877798a6e2d5dc6ef24a76cc6731c3e
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-12-22 21:13:00 -1000

    🐛  BUG: fix userbyid render bug

--- src/Global/Header/HeaderComponent.jsx
+++ src/Global/Header/HeaderComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { getUserById } from '../../actions/actions.js';
 import { connect } from 'react-redux';
 
@@ -9,17 +9,17 @@ let id = localStorage.userId;
 
 class Header extends Component {
   //   constructor(props) {
-    //     super(props)
-    //     this.state = {
-      
-      //     }
-      //   }
+  //     super(props)
+  //     this.state = {
+
+  //     }
+  //   }
   componentDidMount() {
     console.log("Setting localStorage userId:", localStorage.userId);
     this.props.dispatch(getUserById(localStorage.userId));
     this.props.dispatch(getUserById(id));
   }
-  
+
   render() {
     console.log('props!!', this.props)
     if (localStorage.email) {
@@ -27,10 +27,10 @@ class Header extends Component {
         <div className="header">
           <a href="/"><h1>TREK<span>mate</span></h1></a>
 
-          <form method="get" action={`/users/account/${id}`}> 
-          <button className="ghost" type="submit">{this.props.userById.first_name}</button>
+          <form method="get" action={`/users/account/${id}`}>
+            <button className="ghost" type="submit">{this.props.userById.first_name}</button>
           </form>
-    
+
         </div>
       );
     } else {
@@ -54,3 +54,9 @@ const mapStateToProps = state => {
   // return state
 }
 export default connect(mapStateToProps)(Header);
+
+Header.defaultProps = {
+  userById: {
+    id: 0,
+  }
+}
